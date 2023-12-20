8ac06c0cec27aadeacd9f086a4dc4a2e20b14b45
quyen2511 <152406171+quyen2511@users.noreply.github.com>
2023-12-20 12:54:47 +0700

    Hienthiloichao

---
+++ Hienthiloichao/Hienthiloichao/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Hienthiloichao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello: " + yourName);
+        }
+    }
+}
