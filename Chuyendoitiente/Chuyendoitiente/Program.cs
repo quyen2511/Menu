298d28e76cc42fea9a4f09d46ef513b7a8e4cde7
quyen2511 <152406171+quyen2511@users.noreply.github.com>
2023-12-20 13:04:42 +0700

    USDtoVND

---
+++ Chuyendoitiente/Chuyendoitiente/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Chuyendoitiente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const float rate=23000;
+            float USD;
+            Console.Write("Enter USD: ");
+            USD = float.Parse(Console.ReadLine());           
+            float VND = USD * rate;
+            Console.WriteLine("VND: " + VND);
+        }
+    }
+}
