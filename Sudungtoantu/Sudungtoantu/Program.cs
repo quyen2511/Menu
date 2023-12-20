84fbdee8f4e8b9fbd9e9e6c2418c203e0f72936c
quyen2511 <152406171+quyen2511@users.noreply.github.com>
2023-12-20 12:52:54 +0700

    sudungtoantu

---
+++ Sudungtoantu/Sudungtoantu/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Sudungtoantu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+        }
+    }
+}
