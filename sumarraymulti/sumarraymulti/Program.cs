ed00e384791a628cf6e4e92751bc9495303c97bd
quyen2511 <152406171+quyen2511@users.noreply.github.com>
2023-12-27 19:09:22 +0700

    sumarraymulti

---
+++ sumarraymulti/sumarraymulti/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so hang");
+        int row = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap so cot");
+        int colum = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap cac phan tu cua mang");
+        int[,] array = new int[row, colum];
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < colum; j++)
+            {
+                array[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        //in cac phan tu cua mang
+        for (int i = 0; i < row; i++)
+        {
+            Console.WriteLine("\n");
+            for (int j = 0; j < colum; j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+        }
+        //tinh tong
+        int total = 0;
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < colum; j++)
+            {
+                if (i == j)
+                {
+                    total += array[i, j];
+                }
+            }
+        }
+        Console.WriteLine("Gia tri tong duong cheo la " + total);
+    }
+}
