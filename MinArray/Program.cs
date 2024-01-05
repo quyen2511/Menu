73dff66a703883680265bc006b6ba99a468d883a
quyen2511 <152406171+quyen2511@users.noreply.github.com>
2024-01-05 14:41:44 +0700

    timgiatrinhonhat

---
+++ MinArray/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array= {1,2,3,4,5,6,7,8,9,20,21};
+        Console.WriteLine("Gia tri lon nhat la");
+        Console.WriteLine(MinFind(array));
+    }
+    static int MinFind(int[] array)
+    {
+        int min = array[0];
+        for (int i = 0; i < array.Length-1; i++)
+        {
+            if(array[i]>min)
+            {
+                min=array[i];
+            }
+        }
+        return min;
+    }
+}
