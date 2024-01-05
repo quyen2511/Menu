59c46fc75db954f50a560b9d6665d1bd039d4245
quyen2511 <152406171+quyen2511@users.noreply.github.com>
2024-01-05 16:37:29 +0700

    demsolanxuathiencuaphantu

---
+++ Countarray/Countarray/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Countarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao chuoi: ");
+            string message = Console.ReadLine();
+            while (message.Length > 0)
+            {
+                Console.Write(message[0] + " : ");
+                int count = 0;
+                for (int j = 0; j < message.Length; j++)
+                {
+                    if (message[0] == message[j])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+                message = message.Replace(message[0].ToString(), string.Empty);
+            }
+            Console.ReadKey();
+        }
+    }
+}
