7984a99769bb2f87aef0aa62c1e7d10b70e5313a
quyen2511 <152406171+quyen2511@users.noreply.github.com>
2024-01-22 21:56:29 +0700

    ExFileinfoAndStreamreader

---
+++ Sumfiletext/Sumfiletext/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Sumfiletext
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum=0;
+            string sourcepath = @"C:\DATA\map.txt";
+            FileInfo finfo = new FileInfo(sourcepath);
+            if (finfo.Exists)
+            {
+                StreamReader reader = new StreamReader(sourcepath);
+                string line="";               
+                while ((line=reader.ReadLine())!=null)
+                {
+                    Console.WriteLine(line);
+                    sum+= Int32.Parse(line);
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
